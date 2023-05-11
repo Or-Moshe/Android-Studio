24a1110af4ad29c90abbb69b9af035ee0b4161af
OrMoshe <ormoshe2468@gmail.com>
2023-05-11 15:22:23 +0300

    adding score table by array list

--- app/src/main/java/com/example/obscalesraceapp/View/ScoreTableActivity.java
+++ app/src/main/java/com/example/obscalesraceapp/View/ScoreTableActivity.java
@@ -20,21 +20,22 @@ import java.util.stream.Collectors;
 public class ScoreTableActivity extends AppCompatActivity {
 
     private RecyclerView main_LST_scores;
-    private Button new_game_btn, new_user_btn;
-
+    private Button backToMenuPageClicked, new_user_btn;
+    private String current_user_json;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_score_table);
 
+        setParamsFromAnotherIntent();
         findViews();
         initViews();
 
-        this.new_game_btn.setOnClickListener(new View.OnClickListener() {
+        this.backToMenuPageClicked.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view){
-                onNewGameClick();
+                onBackToMenuPageClicked();
             }
         });
 
@@ -48,7 +49,7 @@ public class ScoreTableActivity extends AppCompatActivity {
 
     private void findViews() {
         this.main_LST_scores = (RecyclerView)findViewById(R.id.main_LST_scores);
-        this.new_game_btn = (Button)findViewById(R.id.new_game_btn);
+        this.backToMenuPageClicked = (Button)findViewById(R.id.new_game_btn);
         this.new_user_btn = (Button)findViewById(R.id.new_user_btn);
     }
 
@@ -61,9 +62,9 @@ public class ScoreTableActivity extends AppCompatActivity {
         this.main_LST_scores.setLayoutManager(linearLayoutManager);
     }
 
-    private void onNewGameClick(){
-        Intent myIntent = new Intent(ScoreTableActivity.this, MainActivity.class);
-        //myIntent.putExtra("current_user", current_user); //Optional parameters
+    private void onBackToMenuPageClicked(){
+        Intent myIntent = new Intent(ScoreTableActivity.this, MenuActivityActivity.class);
+        myIntent.putExtra("current_user_json", current_user_json); //Optional parameters
         startActivity(myIntent);
     }
 
@@ -71,5 +72,9 @@ public class ScoreTableActivity extends AppCompatActivity {
         Intent myIntent = new Intent(ScoreTableActivity.this, LoginActivity.class);
         //myIntent.putExtra("current_user", current_user); //Optional parameters
         startActivity(myIntent);
+    }
+
+    private void setParamsFromAnotherIntent(){
+        this.current_user_json = getIntent().getStringExtra("current_user_json");
     }
 }
